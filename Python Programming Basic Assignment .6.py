4c8763aa1bccd92f4c19e55eb579de4cb8692f3e
Harinandan kumar <97117217+Harinandankumar@users.noreply.github.com>
2022-04-22 14:52:44 +0530

    Add files via upload

---
+++ Python Programming Basic Assignment .6.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1.	Write a Python Program to Display Fibonacci Sequence Using Recursion?
+# 
+# 
+# 
+# 
+# 
+# 
+# Ans:-
+
+# In[1]:
+
+
+def genFibonacci(n,a,b):
+    if n == 0:
+        return 1
+    else:
+        result = a+b
+        print(result, end=', ')
+        genFibonacci(n-1,b,result)
+in_num = int(input('Enter the length of Series: '))
+print('0, 1',end=', ')
+genFibonacci(in_num,1,2)
+
+
+# 2.	Write a Python Program to Find Factorial of Number Using Recursion?
+# 
+# 
+# 
+# 
+# 
+# Ans:-
+
+# In[2]:
+
+
+def factorial(num):
+    if (num < 1):
+        return 1
+    else:
+        return num*factorial(num-1)
+num = int(input('Enter a number: '))
+value = factorial(num)
+print(f'The Factorial of {num} is {value}')
+
+
+# 3.	Write a Python Program to calculate your Body Mass Index?
+# 
+# 
+# 
+# 
+# Ans:-
+
+# In[3]:
+
+
+def calculateBMI():
+    in_weight = eval(input('Enter your Weight(kgs): '))
+    in_height = eval(input('Enter your Height(mts): '))
+    calc_bmi = in_weight/pow(in_height,2)
+    if (calc_bmi < 18.5):
+        status = 'Underweight'
+    elif (calc_bmi >= 18.5 and calc_bmi < 24.9):
+        status = 'Healthy'
+    elif (calc_bmi >= 24.9 and calc_bmi < 30):
+        status = 'Overweight'
+    elif (calc_bmi >=30):
+        status = 'Suffering from Obesity'
+    print(f'Your\'re BMI is {calc_bmi} and status is {status} ')
+calculateBMI()
+
+
+# 4.	Write a Python Program to calculate the natural logarithm of any number?
+# 
+# 
+# 
+# 
+# 
+# 
+# Ans:-
+
+# In[4]:
+
+
+import math
+def genNatLog():
+    in_num = eval(input("Enter a Number:"))
+    print(math.log(in_num))
+
+genNatLog()
+
+
+# 5.	Write a Python Program for cube sum of first n natural numbers?
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# Ans:-
+
+# In[5]:
+
+
+def cubeOfNaturalNumbers():
+    in_num = int(input("Enter the no of Natural Numbers: "))
+    result = pow(((in_num * (in_num +1))/2),2)
+    print(f'The Cube Sum of First {in_num} Natural Numbers is {result}')
+
+cubeOfNaturalNumbers()
+
